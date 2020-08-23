e068cc74cdbdfec0bdb2c6511a43b37cec3cdc27
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-08-23 15:38:53 +0200

    sm fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ export default function Header() {
       <h1 onClick={switchDescription}>Pareto's Constats</h1>
       <p style={showDescription ? descriptionStyle.visible : descriptionStyle.invisible}>
         Pareto's Law came from the constat that 80% of the outcome are coming from 20% of the input.
-        It is the most know law of productivity. You can make constats of activities in your life and compare the time spent, the money spent, the emotionnal involvment... Find what's bringing you the most postitive value with the least take away.
+        It is the most well known law of productivity. You can make constats of activities in your life and compare the time spent, the money spent, the emotionnal involvment... Find what's bringing you the most postitive value with the least take away.
       </p>
     </header>
   )
