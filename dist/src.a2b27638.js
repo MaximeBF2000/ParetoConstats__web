6ecf70722077a9aae2ed56186293d900f3ec20da
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-08-23 15:37:41 +0200

    sm fix

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -80787,7 +80787,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52190" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52613" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
